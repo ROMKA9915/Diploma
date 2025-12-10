855d6af05a08cf8eaa9ce6eb18b9537340072886
ROMKA9915 <marciukromawka@mail.ru>
2025-12-10 22:59:40 +0300

    обновил main activity

--- app/src/main/java/com/app/diploma/MainActivity.kt
+++ app/src/main/java/com/app/diploma/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.app.diploma
 
-import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.activity.*
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Scaffold
@@ -14,7 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.app.diploma.data.local.AppSettings
-import com.app.diploma.presentation.navigation.Navigator
+import com.app.diploma.presentation.screens.main.MainScreen
 import com.app.diploma.presentation.theme.DiplomaTheme
 import com.app.diploma.presentation.theme.LocalColors
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,28 +21,31 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject lateinit var prefs: SharedPreferences
     @Inject lateinit var appSettings: AppSettings
-    @Inject lateinit var navigator: Navigator
+
+    private val viewModel by viewModels<MainActivityViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        prefs.registerOnSharedPreferenceChangeListener(appSettings)
+        val mainScreen = MainScreen()
+        viewModel.navigator.replace(mainScreen)
 
         setContent {
             val currentTheme by appSettings.currentTheme.collectAsStateWithLifecycle()
-            val currentScreen by navigator.lastScreenFlow.collectAsStateWithLifecycle()
+            val currentLocale by appSettings.currentLocale.collectAsStateWithLifecycle()
+            val currentScreen by viewModel.navigator.lastScreenFlow.collectAsStateWithLifecycle()
 
             BackHandler {
-                navigator.pop {
+                viewModel.navigator.pop {
                     finish()
                 }
             }
 
             DiplomaTheme(
                 scheme = currentTheme,
+                locale = currentLocale,
             ) {
                 val colors = LocalColors.current
 
